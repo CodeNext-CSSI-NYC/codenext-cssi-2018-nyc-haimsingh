c443cb72f2ed22987c44eb35d941db96eacc527c
haimsingh <41017415+haimsingh@users.noreply.github.com>
2018-07-09 16:31:30 -0400

    answered math-practice

--- hello.js
+++ hello.js
@@ -1,2 +1,7 @@
 // Author: Haim Singh
 console.log("Hello World!");
+console.log("A monster attacks!");
+console.log("Dodge or attack?");
+console.log("Dodge Fail");
+console.log("Took 3 damage");
+console.log("-3");
