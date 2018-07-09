a77995c223ef0ba90211a08e0457da290c98db91
haimsingh <41017415+haimsingh@users.noreply.github.com>
2018-07-09 17:00:39 -0400

    started easter guesser

--- easter-predictor.js
+++ easter-predictor.js
@@ -1,1 +1,14 @@
-// Author: FirstName LastName
+// Author: Haim Singh
+let y = 2019;
+let a = y/19
+let b = y/100
+let c = y%100
+let d = b/4
+let e = b%4
+let f = (b+8)/25
+let g = (b-f+1)/3
+let h = (19 * a + b - d - g + 15)/30
+let i = c/4
+let k = c%4
+let r = (32 + 2 * e + 2 * i - h - k)%7
+ 
