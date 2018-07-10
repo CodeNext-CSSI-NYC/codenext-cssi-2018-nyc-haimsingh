60cc2ad9f8023b570b7c95e5decc353fb9622438
haimsingh <41017415+haimsingh@users.noreply.github.com>
2018-07-10 17:00:37 -0400

    started Cord.

---
+++ Coordinite-plane.js
@@ -0,0 +1,8 @@
+// Author Haim Singh
+
+const readline = require('readline-sync');
+
+let x1 = parseInt(readline.question("What is the first X cordinite? "));
+let y1 = parseInt(readline.question("What is the first Y cordinite? "));
+let x2 = parseInt(readline.question("What is the second X cordinite? "));
+let y1 = parseInt(readline.question("What is the second Y cordinite? "));
