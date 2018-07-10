057cff44cf64cb58aa67669fc629adccf06b8edf
haimsingh <41017415+haimsingh@users.noreply.github.com>
2018-07-10 10:34:06 -0400

    Finished hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,11 @@
 // Author: Haim Singh
-console.log("Hello World!");
-console.log("A monster attacks!");
-console.log("Dodge or attack?");
-console.log("Dodge Fail");
-console.log("Took 3 damage");
-console.log("-3");
+let readline = require("readline-sync");
+let userName = readline.question("Enter your name :");
+console.log("Hello " + userName + "!");
+
+
+// console.log("A monster attacks!");
+// console.log("Dodge or attack?");
+// console.log("Dodge Fail");
+// console.log("Took 3 damage");
+// console.log("-3");
